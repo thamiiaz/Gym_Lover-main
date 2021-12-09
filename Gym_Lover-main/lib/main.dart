a58fb3f08669fffe638dab8dda36b1581fb6358d
Thamires Azevedo <Tt_15@hotmail.com>
2021-12-09 19:02:37 -0300

    tudo

---
+++ Gym_Lover-main/lib/main.dart
@@ -0,0 +1,36 @@
+//import 'package:gym_lover/auth.dart';
+//import 'package:js/js.dart';
+import 'package:flutter/material.dart';
+import 'package:gym_lover/splash_screen.dart';
+
+//import 'avaliacao_fisica.dart';
+
+//import 'package:flutter/services.dart';
+//import 'package:gym_lover/login_screen.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:provider/provider.dart';
+//import 'package:get/get.dart';
+
+
+void main() async{
+
+  runApp(
+    // MultiProvider(providers: [
+  // ChangeNotifierProvider(create: (context) => Auth()),
+  //     ],
+  //     child:
+      MyApp()
+  );
+  
+  // Get.lazyPut<Auth>(() => Auth());
+}
+
+class MyApp extends StatelessWidget {
+
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: splash_screen(),
+    );
+  }
+}
---
+++ Gym_Lover-main/lib/models/exercicio_modal.dart
@@ -0,0 +1,7 @@
+class ExercicioModal {
+  final String? nome;
+  final String? image;
+  final String? series;
+
+  ExercicioModal({this.nome, this.image, this.series});
+}

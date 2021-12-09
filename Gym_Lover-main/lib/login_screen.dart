8c234d1393c89e458e120271d7fbb53df7cb5101
Thamires Azevedo <Tt_15@hotmail.com>
2021-12-09 19:12:30 -0300

    perfeito

--- Gym_Lover-main/lib/login_screen.dart
+++ Gym_Lover-main/lib/login_screen.dart
@@ -11,18 +11,15 @@ import 'Alunos_instrutor.dart';
 // import 'package:gym_lover/Validacao.dart';
 // import "package:provider/provider.dart";
 
-
 class login_screen extends StatefulWidget {
   @override
   _login_screenState createState() => _login_screenState();
 }
 
 class _login_screenState extends State<login_screen> {
-
   final email = TextEditingController();
   final senha = TextEditingController();
   bool loading = false;
-
 
   @override
   void initState() {
@@ -86,17 +83,17 @@ class _login_screenState extends State<login_screen> {
                       borderRadius: BorderRadius.circular(30)),
                   child: TextField(
                     onChanged: (text) {
-                      email = text;},
+                      email = text;
+                    },
                     decoration: InputDecoration(
-                        icon: Icon(Icons.email),
-                        hintText: "E-mail",
-                        enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)),
-                     //   errorText: validacao.emailValidacao(email.text),
+                      icon: Icon(Icons.email),
+                      hintText: "E-mail",
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      //   errorText: validacao.emailValidacao(email.text),
                     ),
                   ),
                 ),
-
                 Container(
                   height: 50,
                   margin: EdgeInsets.only(top: 250, left: 40, right: 40),
@@ -107,7 +104,8 @@ class _login_screenState extends State<login_screen> {
                       borderRadius: BorderRadius.circular(30)),
                   child: TextField(
                     onChanged: (text) {
-                      senha = text;},
+                      senha = text;
+                    },
                     decoration: InputDecoration(
                       icon: Icon(Icons.lock_rounded),
                       hintText: "Senha",
@@ -118,7 +116,6 @@ class _login_screenState extends State<login_screen> {
                     obscureText: true,
                   ),
                 ),
-
                 Container(
                     margin: EdgeInsets.only(top: 320, left: 40, right: 40),
                     padding:
@@ -129,32 +126,43 @@ class _login_screenState extends State<login_screen> {
                       // ignore: deprecated_member_use
                       child: RaisedButton(
                         onPressed: () {
-                          if (senha == "123456" && email == "aluno@gymlover.com") {
+                          if (senha == "123456" &&
+                              email == "aluno@gymlover.com") {
                             Navigator.push(
-                              context, MaterialPageRoute(builder: (context) {
-                              return home_screen();
-                            },),);
-                          } else if(senha == "123456" && email == "personal@gymlover.com"){
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return home_screen();
+                                },
+                              ),
+                            );
+                          } else if (senha == "123456" &&
+                              email == "personal@gymlover.com") {
                             Navigator.push(
-                              context, MaterialPageRoute(builder: (context) {
-                              return aluno_instrutor_screen();
-                            },),);
-                          }},
-                       // onPressed: () {
-                       //   var validatext = validacao.SenhaEmailValidacao(email.text, senha.text);
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return aluno_instrutor_screen();
+                                },
+                              ),
+                            );
+                          }
+                        },
+                        // onPressed: () {
+                        //   var validatext = validacao.SenhaEmailValidacao(email.text, senha.text);
 
-                       //   if (validatext != null){
-                            //**** continuar essa logica !!! *****
-                       //   }else{
-                       //    try {
-                       //      auth.sigin( email.text, senha.text);
-                       //    }on AuthException catch (e){
-                       //      auth.showSnack('Erro na Autenticação', e.toString());
-                       //    } catch (e){
-                       //      auth.showSnack('Erro na Autenticação', 'Erro Inesperado !');
-                       //    }
-                       //   }
-                       // },
+                        //   if (validatext != null){
+                        //**** continuar essa logica !!! *****
+                        //   }else{
+                        //    try {
+                        //      auth.sigin( email.text, senha.text);
+                        //    }on AuthException catch (e){
+                        //      auth.showSnack('Erro na Autenticação', e.toString());
+                        //    } catch (e){
+                        //      auth.showSnack('Erro na Autenticação', 'Erro Inesperado !');
+                        //    }
+                        //   }
+                        // },
                         color: Color(0xFF9f1a1b),
                         shape: RoundedRectangleBorder(
                             borderRadius:
@@ -171,84 +179,91 @@ class _login_screenState extends State<login_screen> {
                         EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
                     alignment: Alignment.center,
                     height: 50,
-                   // width: 700,
+                    // width: 700,
                     child: TextButton(
                       child: Text(
                         'Esqueceu Sua Senha?',
                         style: TextStyle(color: Colors.white),
                       ),
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) {
-                          return ForgotPassword();
-                        }, ),);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return ForgotPassword();
+                            },
+                          ),
+                        );
                       },
                     )),
-
-                    Container(
-                        margin: EdgeInsets.only(top: 500),
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
-                        height: 50,
-                          child: Text(
-                            'Não Tem Uma Conta?',
-                            style: TextStyle(color: Colors.black54),
-                          ),
-                        ),
-
-                    Container(
-                        margin: EdgeInsets.only(top: 560, left: 40, right: 40),
-                        padding:
+                Container(
+                  margin: EdgeInsets.only(top: 500),
+                  alignment: Alignment.center,
+                  padding:
+                      EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
+                  height: 50,
+                  child: Text(
+                    'Não Tem Uma Conta?',
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(top: 560, left: 40, right: 40),
+                    padding:
                         EdgeInsets.only(top: 2, left: 8, right: 8, bottom: 4),
-                        child: SizedBox(
-                          height: 50,
-                          width: 700,
-                          child: RaisedButton(
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    child: SizedBox(
+                      height: 50,
+                      width: 700,
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
                                 return cadastro_screen();
-                              }, ),);
-                            },
-                            color: Color(0xFF9f1a1b),
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(30))),
-                            child: Text(
-                              "Cadastre-se",
-                              style: TextStyle(color: Colors.white),
+                              },
                             ),
-                          ),
-                        )),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.only(top: 630, right: 5, left: 5),
-                            child: IconButton(
-                               icon: Image.asset('assets/image/fb.png'),
-                               iconSize: 30,
-                               onPressed: () {},
-                             ),
+                          );
+                        },
+                        color: Color(0xFF9f1a1b),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        child: Text(
+                          "Cadastre-se",
+                          style: TextStyle(color: Colors.white),
                         ),
-                        Container(
-                          margin: EdgeInsets.only(top: 630, right: 5, left: 5),
-                          child: IconButton(
-                            icon: Image.asset('assets/image/google.png'),
-                            iconSize: 30,
-                            onPressed: () {},
-                          ),
-                        ),
-
-                        Container(
-                          margin: EdgeInsets.only(top: 640, right: 5, left: 5),
-                          child: IconButton(
-                            icon: Image.asset('assets/image/twitter.png'),
-                            iconSize: 30,
-                            onPressed: () {},
-                          ),
-                        ),
-                      ],
+                      ),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 630, right: 5, left: 5),
+                      child: IconButton(
+                        icon: Image.asset('assets/image/fb.png'),
+                        iconSize: 30,
+                        onPressed: () {},
+                      ),
                     ),
+                    Container(
+                      margin: EdgeInsets.only(top: 630, right: 5, left: 5),
+                      child: IconButton(
+                        icon: Image.asset('assets/image/google.png'),
+                        iconSize: 30,
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 640, right: 5, left: 5),
+                      child: IconButton(
+                        icon: Image.asset('assets/image/twitter.png'),
+                        iconSize: 30,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
               ])),
             ),
           ]),

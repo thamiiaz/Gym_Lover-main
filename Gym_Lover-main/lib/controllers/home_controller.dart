9465039fe6fc6e0fc92397956c6f3b5ada46cf55
Thamires Azevedo <Tt_15@hotmail.com>
2021-12-09 18:58:31 -0300

    mensagem

---
+++ Gym_Lover-main/lib/controllers/home_controller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:gym_lover/models/exercicio_modal.dart';
+import 'package:gym_lover/models/treino_model.dart';
+
+class HomeController extends ChangeNotifier {
+  final email = TextEditingController();
+  final senha = TextEditingController();
+  bool loading = false;
+  List<TreinoModel> treinos = [
+    TreinoModel(
+      nome: "TREINO 1",
+      imagem: "assets/image/1.jpg",
+      exercicios: [
+        ExercicioModal(nome: "Abdominal", image: "assets/image/absupra.jpg"),
+      ],
+    ),
+    TreinoModel(
+      nome: "TREINO 2",
+      imagem: "assets/image/2.jpg",
+      exercicios: [
+        ExercicioModal(nome: "Esteira", image: "assets/image/esteira.jpg"),
+      ],
+    ),
+    TreinoModel(
+      nome: "TREINO 3",
+      imagem: "assets/image/3.jpg",
+      exercicios: [
+        ExercicioModal(nome: "Abdominal", image: "assets/image/poli.gif"),
+      ],
+    ),
+  ];
+
+  // final width = MediaQuery.of(context).size.width;
+  // Auth auth = Provider.of(context, listen: false);
+  // Validacao validacao = new Validacao();
+
+  void limparTreinos() {
+    treinos.clear();
+    notifyListeners();
+  }
+}
